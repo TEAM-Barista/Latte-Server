b5a57518a2b7c37e5b6aa78343dad34927eed743
Lee Donggun <tnpdlzi@naver.com>
2021-07-14 16:58:06 +0900

    feat: 레포지토리 추가

---
+++ src/main/java/com/latte/server/common/repository/PostRepository.java
@@ -0,0 +1,32 @@
+package com.latte.server.common.repository;
+
+import com.latte.server.common.domain.Post;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+
+    private final EntityManager em;
+
+    public void save(Post post) {
+        em.persist(post);
+    }
+
+    public Post findOne(Long id) {
+        return em.find(Post.class, id);
+    }
+
+    // fetch join
+    public List<Post> findAll() {
+        return em.createQuery(
+                "select p from Post p" +
+                        " join fetch p.user u", Post.class
+        ).getResultList();
+    }
+
+}
---
+++ src/main/java/com/latte/server/common/repository/UserRepository.java
@@ -0,0 +1,35 @@
+package com.latte.server.common.repository;
+
+import com.latte.server.common.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+
+    private final EntityManager em;
+
+    public void save(User user) {
+        em.persist(user);
+    }
+
+    public User findOne(Long id) {
+        return em.find(User.class, id);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+
+    public List<User> findByName(String name) {
+        return em.createQuery("select u from User u where u.userName = :name", User.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
