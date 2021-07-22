b04721e684ebeb92ae8599356388d45f097fa7fa
Lee Donggun <tnpdlzi@naver.com>
2021-07-22 17:43:08 +0900

    test: 댓글 갯수, 좋아요 갯수 count 테스트 추가

---
+++ src/test/java/com/latte/server/post/repository/PostRepositoryTest.java
@@ -0,0 +1,71 @@
+package com.latte.server.post.repository;
+
+import com.latte.server.post.service.PostService;
+import com.latte.server.user.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class PostRepositoryTest {
+
+    @Autowired
+    EntityManager em;
+    @Autowired
+    PostService postService;
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void 댓글_갯수_카운트() {
+        //given
+        User user = createUser();
+        String postContent = "test content";
+        String postTitle = "test title";
+        String postCode = "#stdio.h";
+        int isQna = 0;
+        Long postId = postService.post(user.getId(), postContent, postTitle, postCode, isQna);
+
+        //when
+        int count = postRepository.countReplies(postId);
+
+        //then
+        Assertions.assertThat(count).isEqualTo(0);
+    }
+
+    @Test
+    public void 좋아요_갯수_카운트() {
+        //given
+        User user = createUser();
+        String postContent = "test content";
+        String postTitle = "test title";
+        String postCode = "#stdio.h";
+        int isQna = 0;
+        Long postId = postService.post(user.getId(), postContent, postTitle, postCode, isQna);
+
+        //when
+        int count = postRepository.countPostLikes(postId);
+
+        //then
+        Assertions.assertThat(count).isEqualTo(0);
+    }
+
+    private User createUser() {
+        User user = new User();
+        user.setUserName("user1");
+        user.setUserId("test");
+        user.setEmail("test@test.com");
+        user.setIntro("test intro");
+        em.persist(user);
+        return user;
+    }
+}
