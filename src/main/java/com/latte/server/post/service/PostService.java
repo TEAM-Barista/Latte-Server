44cc0681be602494b266edb05c8c17e17669f02f
Lee Donggun <tnpdlzi@naver.com>
2021-08-18 22:15:59 +0900

    feat: post 추가시 title, content 공백 여부 검증 로직 추가

--- src/main/java/com/latte/server/post/service/PostService.java
+++ src/main/java/com/latte/server/post/service/PostService.java
@@ -8,6 +8,10 @@ import com.latte.server.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import static org.springframework.util.StringUtils.*;
+
 
 /**
  * Created by Donggun on 2021-08-05
@@ -18,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class PostService {
     private static final String NOT_EXIST_USER = "[ERROR] No such User";
+    private static final String NOT_EXIST_TEXT = "[ERROR] Do not contain text";
 
     private final UserRepository userRepository;
     private final PostRepository postRepository;
@@ -28,7 +33,9 @@ public class PostService {
     @Transactional
     public Long post(Long userId, String postContent, String postTitle, String postCode, int isQna) {
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException(NOT_EXIST_USER));
-
+        if (!(hasText(postTitle) && hasText(postContent))) {
+            throw new IllegalArgumentException(NOT_EXIST_TEXT);
+        }
 
         Post post = Post.createPost(user, postContent, postTitle, postCode, isQna);
 
