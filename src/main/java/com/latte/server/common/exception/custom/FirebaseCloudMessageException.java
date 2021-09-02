26a65ed1e46ce99e4c8839e21e352825c6ef144a
koreaaerospaceU <zx811@naver.com>
2021-09-03 02:10:10 +0900

    feat: FCM 유틸리티 제작 - FCM 초기 설정 - FCM 토픽 메시지 전송 로직 추가 - FCM 토큰 메시지 전송 로직 추가 - Firebase Exception 추가

---
+++ src/main/java/com/latte/server/common/exception/custom/FirebaseCloudMessageException.java
@@ -0,0 +1,7 @@
+package com.latte.server.common.exception.custom;
+
+/**
+ * Created by Minky on 2021-09-03
+ */
+public class FirebaseCloudMessageException {
+}
---
+++ src/main/java/com/latte/server/push/util/FirebaseCloudMessageUtility.java
@@ -0,0 +1,7 @@
+package com.latte.server.push.util;
+
+/**
+ * Created by Minky on 2021-09-03
+ */
+public class FirebaseCloudMessageUtility {
+}
