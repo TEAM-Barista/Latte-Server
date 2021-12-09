57a51e8eb672a7da407755c0e609ca11cbc032a7
koreaaerospaceU <zx811@naver.com>
2021-12-09 22:05:43 +0900

    feat: 알림 타겟 컨스턴트 클래스 제작

---
+++ src/main/java/com/latte/server/push/domain/PushTarget.java
@@ -0,0 +1,13 @@
+package com.latte.server.push.domain;
+
+/**
+ * Created by Minky on 2021-12-09
+ */
+public final class PushTarget {
+    private PushTarget() { }
+
+    public static String INTERVIEW = "interview";
+    public static String QNA = "post";
+    public static String POST = "post";
+    public static String REPLY = "post/reply";
+}
