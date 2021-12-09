250c83ea284e578c65f248fb6d47b17a2b7881ad
koreaaerospaceU <zx811@naver.com>
2021-12-09 22:48:26 +0900

    feat: PushToken 도메인 사용자 검색 추상화 메소드 정의

--- src/main/java/com/latte/server/push/repository/PushTokenRepository.java
+++ src/main/java/com/latte/server/push/repository/PushTokenRepository.java
@@ -1,10 +1,14 @@
 package com.latte.server.push.repository;
 
 import com.latte.server.push.domain.PushToken;
+import com.latte.server.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 /**
  * Created by Minky on 2021-09-03
  */
 public interface PushTokenRepository extends JpaRepository<PushToken, Long> {
+    Optional<PushToken> findByUser(User user);
 }
